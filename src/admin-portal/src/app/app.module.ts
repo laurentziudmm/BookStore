e9eb82be5c84fb5919e0701c889d377ec08a48fc
laurentziudmm <laurentziudmm@gmail.com>
2020-01-24 19:38:11 +0200

    Nav Bar Component Added

--- src/admin-portal/src/app/app.module.ts
+++ src/admin-portal/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
