e6e8c4e75cbe39386e1fbe05be7dca6da6aa19ae
laurentziudmm <laurentziudmm@gmail.com>
2020-01-26 15:00:26 +0200

    Adding Login Template

--- src/admin-portal/src/app/services/login.service.ts
+++ src/admin-portal/src/app/services/login.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http , Headers } from '@angular/common/http';
+import { Http , Headers } from '@angular/http';
 // import { Observable } from 'rxjs/Observable';
 
-@Injectable ({
-  providedIn: 'root'
-})
+@Injectable ({ providedIn : 'root' })
 export class LoginService {
 
   constructor(private http: Http) { }
