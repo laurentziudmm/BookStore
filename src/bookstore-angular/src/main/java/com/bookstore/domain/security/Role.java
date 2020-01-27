8f579674ddb1652fdb6970b384f9792f9c398d79
laurentziudmm <laurentziudmm@gmail.com>
2020-01-27 18:12:24 +0200

    Added Authority and Role Class

---
+++ src/bookstore-angular/src/main/java/com/bookstore/domain/security/Role.java
@@ -0,0 +1,11 @@
+package com.bookstore.domain.security;
+
+import java.io.Serializable;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@Entity
+public class Role implements Serializable {
+    
+}
