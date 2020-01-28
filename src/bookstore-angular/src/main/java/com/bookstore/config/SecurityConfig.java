5a3a4f9f30e24b342d07c15cbe6e347a25228680
laurentziudmm <laurentziudmm@gmail.com>
2020-01-28 10:33:39 +0200

    Added Security Config

---
+++ src/bookstore-angular/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.bookstore.config;
+
+import org.apache.catalina.security.SecurityUtil;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//Annotation enable spring security
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private Environment env ;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
+
+    private BCryptPasswordEncoder passwordEncoder() {
+        return SecurityUtility.passwordEncoder();
+    }
+
+    private static final String[] PUBLIC_MATCHERS = {
+        "/css/**",
+        "/js/**",
+        "/image/**",
+        "/book/**",
+        "/user/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable().httpBasic().and().authorizeRequests()
+        .antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+
+    }
+
+}
