46ca199d355e6c482ab3a90f5ff3ad4718c52d6a
laurentziudmm <laurentziudmm@gmail.com>
2020-01-28 08:52:58 +0200

    updated Set and Get for UserRole

--- src/bookstore-angular/src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/bookstore-angular/src/main/java/com/bookstore/domain/security/UserRole.java
@@ -3,10 +3,13 @@ package com.bookstore.domain.security;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Table(name = "user_role")
@@ -17,5 +20,137 @@ public class UserRole implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long userRoleId;
+
+    // Define a constructor
+    public UserRole() {
+    }
+
+    public UserRole(User user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    // Chenage EAGER to LAZY when you have many users
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Role role;
+
+    public Object get @Id()
+    {
+		return this.@Id;
+	}
+
+    public void set @Id(Object @Id)
+    {
+		this.@Id = @Id;
+	}
+
+    public=getGenerationType.AUTO)()
+    {
+		return this.GenerationType.AUTO);
+	}
+
+    public void setGenerationType.AUTO)(=GenerationType.AUTO))
+    {
+		this.GenerationType.AUTO) = GenerationType.AUTO);
+	}
+
+    public long getUserRoleId() {
+        return this.userRoleId;
+    }
+
+    public void setUserRoleId(long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public a getConstructor() {
+        return this.constructor;
+    }
+
+    public void setConstructor(a constructor) {
+        this.constructor = constructor;
+    }
+
+    public UserRole get()() {
+		return this.();
+	}
+
+    public void set()(UserRole ()) {
+		this.() = ();
+	}
+
+    public UserRole get(User() {
+		return this.(User;
+	}
+
+    public void set(User(UserRole (User) {
+		this.(User = (User;
+	}
+
+	public =
+
+    getUser() {
+		return this.user;
+	}
+
+    public void setUser(= user) {
+		this.user = user;
+	}
+
+	public =
+
+    getRole() {
+		return this.role;
+	}
+
+    public void setRole(= role) {
+		this.role = role;
+	}
+
+    public Object get}(){return this.};}
+
+    public void set}(Object}){this.}=};}
+
+    public EAGER getTo() {
+        return this.to;
+    }
+
+    public void setTo(EAGER to) {
+        this.to = to;
+    }
+
+    public(fetch get=() {
+		return this.=;
+	}
+
+    public void set=((fetch =) {
+		this.= = =;
+	}
+
+	public = get"User_id")() {
+		return this."user_id");
+	}
+
+    public void set"User_id")(="user_id")){this."user_id")="user_id");
+    }
+
+    public User getUser() {
+		return user;
+	}
+
+    public void setUser(User user) {
+		this.user = user;
+	}
+
+    public Role getRole() {
+		return role;
+	}
+
+    public void setRole(Role role) {
+		this.role = role;
+	}
 
 }
